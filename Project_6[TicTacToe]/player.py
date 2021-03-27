48965a10c3d488b858564e5a2e4fc87c4b4ad2b9
Abayomi Shosilva <ashosilva@gmail.com>
2021-03-26 23:20:51 -0400

    TicTacToe

--- Project_6[TicTacToe]/player.py
+++ Project_6[TicTacToe]/player.py
@@ -9,7 +9,7 @@ class Player:
         # letter is x or o
         self.letter = letter
 
-    # we want all players to get their next move
+    # we want all players to get their next move given a game
     def get_move(self, game):
         pass
 
@@ -19,7 +19,9 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        # picks a random valid spot
+        square = random.choice(game.available_moves())
+        return square
 
 
 class HumanPlayer(Player):
@@ -27,5 +29,18 @@ class HumanPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-8):')
 
+            # checking if the input is an integer and if the selceted spot is available
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again')
+
+        return val
